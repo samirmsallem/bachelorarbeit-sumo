1cfcc688854738bc0b3cb1313e46f89455d20ce0
Samir M'Sallem <samir.msallem@valtech-mobility.com>
2022-12-29 16:18:07 +0100

    add multiple super vehicles, extract simulation calls to own manager file

--- api/sim/manager.py
+++ api/sim/manager.py
@@ -1,6 +1,62 @@
+from sumolib import checkBinary 
 import traci
-from api.output import logger
 import re
+import sys
+from api.tli import traffic_light as traffic_light_manager
+from api.tli import glosa as glosa_manager
+from api.output import logger
+
+
+def get_super_vehicles(): # returns list of all active super vehicles (vehicles that should be influenced by glosa and other vehicle information)
+  super_vehicles = []
+
+  for vehicle in traci.vehicle.getIDList():
+    if vehicle.startswith("super"):
+      super_vehicles.append(vehicle)
+
+  return super_vehicles
+
+
+def run_sim():
+    step = 0 # step in simulation count
+    ttc = 0 # time to change (traffic light event)
+    while traci.simulation.getMinExpectedNumber() > 0:
+
+        if(ttc <= 0): # handle traffic lights at intersection
+            state, ttc = traffic_light_manager.get_current_phases()
+            traci.trafficlight.setRedYellowGreenState('tli', state)
+
+        if(step > 0 and step % 3 == 0): # handle vehicles following green light optimal speed advisory (glosa)
+            for vehicle in get_super_vehicles():
+                if(vehicle_did_not_cross_intersection(vehicle)):
+                    x, y = traci.vehicle.getPosition(vehicle)
+                    lon, lat = traci.simulation.convertGeo(x, y)
+                    angle = traci.vehicle.getAngle(vehicle)
+
+                    speed, decision = glosa_manager.glosa_for_position(lat, lon, angle, 30)
+                    print("###### Vehicle " + vehicle + " ######")
+                    print("Calculated speed: " + str(speed) + " km/h with recommendation: " + decision)
+                    traci.vehicle.setSpeed(vehicle, speed / 3.6)
+                else:
+                    traci.vehicle.setSpeed(vehicle, -1)  
+
+        logger.print("Next traffic signal event in " + str(ttc) + "seconds.")       
+
+        traci.simulationStep()
+
+        step += 1
+        ttc -= 1
+
+    traci.close()
+    sys.stdout.flush()
+
+
+
+def start_simulation(sumo_config_file):
+    sumoBinary = checkBinary('sumo-gui')
+    traci.start([sumoBinary, "-c", sumo_config_file])
+    run_sim()
+
 
 def vehicle_did_not_cross_intersection(vehicle): # check whether vehicle crossed intersection to prevent further requests
     pattern = r'^approach_' # regex to find approach edges
--- launch.py
+++ launch.py
@@ -1,3 +1,4 @@
+from api.sim import manager as simulation_manager
 import os
 import sys
 
@@ -8,48 +9,5 @@ if 'SUMO_HOME' in os.environ:
 else:
     sys.exit("'SUMO_HOME' var not defined")
 
-
-from sumolib import checkBinary 
-import traci
-from api.tli import traffic_light as traffic_light_manager
-from api.tli import glosa as glosa_manager
-from api.sim import manager as simulation_manager
-from api.output import logger
-
-def run():
-    step = 0 # step in simulation count
-    ttc = 0 # time to change (traffic light event)
-    while traci.simulation.getMinExpectedNumber() > 0:
-
-        if(ttc <= 0): # handle traffic lights at intersection
-            state, ttc = traffic_light_manager.get_current_phases()
-            traci.trafficlight.setRedYellowGreenState('tli', state)
-
-        if(step > 0): # handle vehicles following green light optimal speed advisory (glosa)
-            for vehicle in traci.vehicle.getIDList():
-                if(simulation_manager.vehicle_did_not_cross_intersection(vehicle)):
-                    x, y = traci.vehicle.getPosition(vehicle)
-                    lon, lat = traci.simulation.convertGeo(x, y)
-                    angle = traci.vehicle.getAngle(vehicle)
-
-                    speed, decision = glosa_manager.glosa_for_position(lat, lon, angle, 30)
-                    logger.print("###### Vehicle " + vehicle + " ######")
-                    logger.print("Calculated speed: " + str(speed) + " km/h with recommendation: " + decision)
-                    traci.vehicle.setSpeed(vehicle, speed / 3.6)
-                else:
-                    traci.vehicle.setSpeed(vehicle, -1)  
-
-        logger.print("Next traffic signal event in " + str(ttc) + "seconds.")       
-
-        traci.simulationStep()
-        step += 1
-        ttc -= 1
-    traci.close()
-    sys.stdout.flush()
-
-
 if __name__ == "__main__":
-    sumoBinary = checkBinary('sumo-gui')
-
-    traci.start([sumoBinary, "-c", "config/osm.sumocfg"])
-    run()
+    simulation_manager.start_simulation("config/osm.sumocfg")
